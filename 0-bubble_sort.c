da94118c726d810b710cd832c32bc8c0271f54e0
korantengtony2 <korantengtony2@gmail.com>
2023-04-25 14:52:10 -0700

    task13

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,11 +36,11 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bubbly = false;
-			}
-		}
-		len--;
+			swap_ints(array + i, array + i + 1);
+		print_array(array, size);
+			bubbly = false;
+	}
+	}
+	len--;
 	}
 }
